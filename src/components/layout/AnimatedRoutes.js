e6233f9647ae75cd8bf59403ea0e0e9811c59bab
Fady Emad <fadyemadfawzygoher258456@gmail.com>
2022-05-09 12:33:40 +0200

    Lazy-loading is added

--- src/components/layout/AnimatedRoutes.js
+++ src/components/layout/AnimatedRoutes.js
@@ -1,11 +1,16 @@
+import React, { Suspense } from 'react';
+
 import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { AnimatePresence, motion } from 'framer-motion';
 
-import Home from '../../pages/Home';
-import Skills from '../../pages/Skills';
-import Education from '../../pages/Education';
-import Projects from '../../pages/Projects';
-import Contact from '../../pages/Contact';
+import classes from './Main.module.css';
+import Loader from '../ui/Loader';
+
+const Home = React.lazy(() => import('../../pages/Home'));
+const Skills = React.lazy(() => import('../../pages/Skills'));
+const Education = React.lazy(() => import('../../pages/Education'));
+const Projects = React.lazy(() => import('../../pages/Projects'));
+const Contact = React.lazy(() => import('../../pages/Contact'));
 
 const routes = [
   { path: '', element: <Navigate replace to="/home" /> },
@@ -22,26 +27,28 @@ const AnimatedRoutes = () => {
 
   return (
     <AnimatePresence exitBeforeEnter>
-      <Routes location={location} key={location.pathname}>
-        <Route path="/">
-          {routes.map(({ path, element }) => (
-            <Route
-              key={path}
-              path={path}
-              element={
-                <motion.section
-                  style={{ marginBottom: '30px' }}
-                  intial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0, transition: { duration: 0.2 } }}
-                >
-                  {element}
-                </motion.section>
-              }
-            />
-          ))}
-        </Route>
-      </Routes>
+      <Suspense fallback={<Loader className={classes.loader} />}>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/">
+            {routes.map(({ path, element }) => (
+              <Route
+                key={path}
+                path={path}
+                element={
+                  <motion.section
+                    style={{ marginBottom: '30px' }}
+                    intial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0, transition: { duration: 0.2 } }}
+                  >
+                    {element}
+                  </motion.section>
+                }
+              />
+            ))}
+          </Route>
+        </Routes>
+      </Suspense>
     </AnimatePresence>
   );
 };
